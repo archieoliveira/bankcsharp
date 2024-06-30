4e5df11aeae6c424ea4b506002b5af82e5264165
Artur Oliveira <arturolivv@gmail.com>
2024-06-30 10:25:53 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Headers;
+using System.Net.WebSockets;
+using System.Globalization;
+using System.Formats.Asn1;
+using System;
+
+namespace exerciciosCurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Entre o valor do depósito inicial");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+        } 
+        
+        
+    }
+}
